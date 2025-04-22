127533a1533b1d195c6ffeba6ffcac8787540ecf
kubaparol <kubciop33@gmail.com>
2025-04-22 15:56:46 +0200

    feat: Enhance AppLayout with dynamic breadcrumbs and page metadata integration for improved navigation and user experience

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 
 import {
   SidebarInset,
@@ -8,6 +8,18 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "./components/AppSidebar";
+import { useParams, usePathname } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { pageMetadataMap } from "@/constants";
 
 interface AppLayoutProps {
   readonly children: ReactNode;
@@ -16,16 +28,70 @@ interface AppLayoutProps {
 export function AppLayout(props: AppLayoutProps) {
   const { children } = props;
 
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routeTemplate = params.id
+    ? pathname.replace(params.id as string, "[id]")
+    : pathname;
+
+  const pageMetadata =
+    pageMetadataMap[routeTemplate as keyof typeof pageMetadataMap];
+
   return (
     <SidebarProvider>
       <AppSidebar variant="sidebar" />
 
       <SidebarInset>
         <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+
+            <Separator orientation="vertical" className="mr-2 !h-4" />
+
+            {pageMetadata?.breadcrumbs.length > 0 && (
+              <Breadcrumb className="text-sm">
+                <BreadcrumbList>
+                  {pageMetadata.breadcrumbs.map((breadcrumb, index) => (
+                    <Fragment key={index}>
+                      <BreadcrumbItem>
+                        {breadcrumb.isCurrent ? (
+                          <BreadcrumbPage>{breadcrumb.label}</BreadcrumbPage>
+                        ) : (
+                          <BreadcrumbLink asChild>
+                            <Link href={breadcrumb.href}>
+                              {breadcrumb.label}
+                            </Link>
+                          </BreadcrumbLink>
+                        )}
+                      </BreadcrumbItem>
+                      {index < pageMetadata.breadcrumbs.length - 1 && (
+                        <BreadcrumbSeparator />
+                      )}
+                    </Fragment>
+                  ))}
+                </BreadcrumbList>
+              </Breadcrumb>
+            )}
+          </div>
         </header>
 
         <div className="flex-1 px-4 py-4 flex flex-col md:gap-6 md:py-6 lg:px-6">
+          {(pageMetadata.title || pageMetadata.description) && (
+            <header>
+              {pageMetadata?.title && (
+                <h1 className="text-2xl font-bold tracking-tight">
+                  {pageMetadata.title}
+                </h1>
+              )}
+
+              {pageMetadata?.description && (
+                <p className="text-muted-foreground">
+                  {pageMetadata.description}
+                </p>
+              )}
+            </header>
+          )}
           {children}
         </div>
       </SidebarInset>
---
+++ src/constants/page-metadata-map.ts
@@ -0,0 +1,56 @@
+type Metadata = {
+  title?: string;
+  description?: string;
+  breadcrumbs: {
+    label: string;
+    href: string;
+    isCurrent?: boolean;
+  }[];
+};
+
+export const pageMetadataMap: Record<string, Metadata> = {
+  "/training-plans": {
+    title: "Training Plans",
+    description: "View and manage your training plans",
+    breadcrumbs: [
+      {
+        label: "Training Plans",
+        href: "/training-plans",
+        isCurrent: true,
+      },
+    ],
+  },
+  "/training-plans/create": {
+    breadcrumbs: [
+      {
+        label: "Create Training Plan",
+        href: "/training-plans/create",
+        isCurrent: true,
+      },
+    ],
+  },
+  "/training-plans/[id]": {
+    breadcrumbs: [
+      {
+        label: "Training Plans",
+        href: "/training-plans",
+      },
+      {
+        label: "Details",
+        href: "/training-plans/[id]",
+        isCurrent: true,
+      },
+    ],
+  },
+  "/settings": {
+    title: "Settings",
+    description: "Manage your account settings",
+    breadcrumbs: [
+      {
+        label: "Settings",
+        href: "/settings",
+        isCurrent: true,
+      },
+    ],
+  },
+};
