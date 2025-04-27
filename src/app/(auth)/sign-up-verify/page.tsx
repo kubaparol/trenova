66f42a3238977d5c0f9bf98a842252d90cd1a765
kubaparol <kubciop33@gmail.com>
2025-04-27 19:24:33 +0200

    feat: Implement sign-up verification page and view

---
+++ src/app/(auth)/sign-up-verify/page.tsx
@@ -0,0 +1,18 @@
+import { SignUpVerifyView } from "@/components/views/SignUpVerifyView";
+import { ProjectUrls } from "@/constants";
+import { supabaseClient } from "@/db/supabase.server";
+import { redirect } from "next/navigation";
+export const runtime = "edge";
+
+export default async function SignUpSuccessPage() {
+  const supabase = await supabaseClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect(ProjectUrls.home);
+  }
+
+  return <SignUpVerifyView />;
+}
---
+++ src/components/views/SignUpVerifyView.tsx
@@ -0,0 +1,16 @@
+import { ProjectUrls } from "@/constants";
+import { MailCheck } from "lucide-react";
+import { MessageCard } from "@/components/shared/MessageCard";
+
+export function SignUpVerifyView() {
+  return (
+    <MessageCard
+      icon={MailCheck}
+      title="Check Your Email"
+      description="We've sent you a confirmation email. Please check your inbox and click the verification link to activate your account."
+      additionalContent="If you don't see the email, please check your spam folder. The link will expire in 24 hours."
+      linkHref={ProjectUrls.signIn}
+      linkText="Back to sign in"
+    />
+  );
+}
